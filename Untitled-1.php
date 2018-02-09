d1eeed6f81914530536da08b69b7346c7cfe7b95
HujMuj <36156401+HujMuj@users.noreply.github.com>
2018-02-09 14:18:08 +0000

    Add files via upload

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,9 +1,5 @@
 <?php
-$result=mail("kovirjalka@mail.ru","Mesage",Message: \nName: $_POST[name]);
-if ($result) {
-    echo "<p> sent</p?";
-}
-else {
-    echo "<p> fuckt</p>";
-}
+
+echo $_GET['password'];
+
 ?>
